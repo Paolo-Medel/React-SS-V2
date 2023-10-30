14201426dc5844d4032f24078021669995bc3d41
Paolo Medel <paolomedel17@gmail.com>
2023-10-30 14:28:27 -0400

    created Ships view iwth delete functionallity and hauler view with edit fuctionality

---
+++ src/components/EditHaulers.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { editHauler, getHaulerById } from "../services/haulerService";
+import { useNavigate, useParams } from "react-router-dom";
+import { getDocks } from "../services/dockService";
+
+export const EditHaulers = () => {
+  // first, I'll need to create a function that retrieves the specific hauler. Put that in a use effect and set state with the data
+  const [hauler, setHauler] = useState({});
+  const [docks, setDocks] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    getHaulerById(parseInt(id)).then((obj) => {
+      setHauler(obj);
+    });
+
+    getDocks().then((obj) => {
+      setDocks(obj);
+    });
+  }, [id]);
+
+  // Third, create the function that actually updates with a put request
+  const navigate = useNavigate();
+
+  const handleSave = () => {
+    let editedHauler = {
+      id: parseInt(id),
+      name: hauler.name,
+      dock_id: hauler.dock_id,
+    };
+
+    editHauler(editedHauler).then(() => {
+      navigate(`/Haulers/`);
+    });
+  };
+  // Second, create the html with forms and pre populate the values.
+  return (
+    <>
+      <div>
+        <div>
+          <label>Name: </label>
+          <input
+            type="text"
+            required
+            value={hauler.name}
+            onChange={(event) => {
+              const copy = { ...hauler };
+              copy.name = event.target.value;
+              setHauler(copy);
+            }}
+          ></input>
+        </div>
+        <div>
+          <label>Dock: </label>
+          <select
+            onChange={(event) => {
+              const copy = { ...hauler };
+              copy.dock_id = parseInt(event.target.value);
+              setHauler(copy);
+            }}
+          >
+            <option value="none" selected disabled hidden>
+              Select an Option
+            </option>
+            {docks.map((obj) => {
+              return (
+                <option key={obj.id} value={obj.id}>
+                  {obj.location}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <button
+          onClick={() => {
+            handleSave();
+          }}
+        >
+          Save Changes
+        </button>
+      </div>
+    </>
+  );
+};
--- src/services/haulerService.jsx
+++ src/services/haulerService.jsx
@@ -1,3 +1,17 @@
 export const getHaulers = () => {
   return fetch(`http://localhost:8000/haulers`).then((res) => res.json());
 };
+
+export const getHaulerById = (id) => {
+  return fetch(`http://localhost:8000/haulers/${id}`).then((res) => res.json());
+};
+
+export const editHauler = (hauler) => {
+  return fetch(`http://localhost:8000/haulers/${hauler.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(hauler),
+  });
+};
